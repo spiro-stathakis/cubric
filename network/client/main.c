c601cc69b3d5571b874dd9b7112cad25c220a72a
Spiro Stathakis <spiro@cardiff.ac.uk>
2013-04-16 11:31:38 +0100

    extended timeout of client

--- network/client/main.c
+++ network/client/main.c
@@ -9,14 +9,9 @@
 
 int main(int argc , char *argv[] )
 {
-
-
 	char *hostname = (argc >= 2) ? argv[1] : "localhost"; 
-	
 	char *portname = (argc == 3) ? argv[2] : "cubricd"; 
 	/* 60000  by default*/ 
-	
-	
 	struct addrinfo hints;
 	memset(&hints,0,sizeof(hints));
 	hints.ai_family=AF_UNSPEC;
@@ -24,25 +19,17 @@ int main(int argc , char *argv[] )
 	hints.ai_protocol=0;
 	hints.ai_flags=AI_ADDRCONFIG;
 	struct addrinfo* res=0;
-	
 	struct timeval timeout;      
 	timeout.tv_sec = 0;
-	timeout.tv_usec = 100;
-
-	
-    
+	timeout.tv_usec = 1000;
 	int err=getaddrinfo(hostname,portname,&hints,&res);
+
 	if (err != 0) {
 		fprintf(stderr,"Error: %s\n",gai_strerror(err));
 		return err; 
 	}
-	
-		
 
 	int sockfd=socket(res->ai_family,res->ai_socktype,res->ai_protocol);
-	
-		
-	
 	
 	if (sockfd==-1) {
 		fprintf(stderr,"Socket error: %s\n",strerror(errno));
@@ -56,9 +43,7 @@ int main(int argc , char *argv[] )
 		return errno; 
 	}	
 	
-	
 	freeaddrinfo(res);
-	
 	char buffer[512];
 	for (;;) {
 		ssize_t count=read(sockfd,buffer,sizeof(buffer));
@@ -75,8 +60,5 @@ int main(int argc , char *argv[] )
 		}
 	}
 	close(sockfd);
-	
 	return 0; 
-	
-
 } 
